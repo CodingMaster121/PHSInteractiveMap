2fd6a94a7e1e9c989e81a54bdf3cd3c59e21466f
CodingMaster121 <ytryanlo@gmail.com>
2024-05-23 07:56:46 -0400

    FIxed id issue with denied perms

--- static/interactive_map.js
+++ static/interactive_map.js
@@ -1,6 +1,6 @@
 const searchAPIUrl = "https://anonymouscoder777.pythonanywhere.com/search";
 const saveLocationUrl = "https://anonymouscoder777.pythonanywhere.com/saveLocation";
-const developerMode = false;
+const developerMode = true;
 const minLatitude = 39.1423;
 const maxLatitude = 39.144609;
 const minLongitude = -77.419817;
