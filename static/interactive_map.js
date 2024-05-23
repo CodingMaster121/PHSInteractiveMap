3bd152f5b2885d92fa46eed0ea205d4ad9c44811
CodingMaster121 <ytryanlo@gmail.com>
2024-05-23 00:16:29 -0400

    More rooms yippee

--- static/interactive_map.js
+++ static/interactive_map.js
@@ -4,10 +4,11 @@ const developerMode = true;
 const minLatitude = 39.1423;
 const maxLatitude = 39.144609;
 const minLongitude = -77.419817;
-const maxLongitude = -77.4185;
+const maxLongitude = -77.41845;
 var currentLatitude = 0;
 var currentLongitude = 0;
 var currentAltitude = 0;
+var searchUpdateQueue = 0;
 
 var locationSettings = {
     enableHighAccuracy: true,
