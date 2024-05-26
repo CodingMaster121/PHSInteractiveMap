7c783945f0143b3466eb4275bdbe42c86d7080cc
CodingMaster121 <ytryanlo@gmail.com>
2024-05-26 01:13:54 -0400

    Developer mode off

--- static/interactive_map.js
+++ static/interactive_map.js
@@ -1,6 +1,6 @@
 const searchAPIUrl = "https://anonymouscoder777.pythonanywhere.com/search";
 const saveLocationUrl = "https://anonymouscoder777.pythonanywhere.com/saveLocation";
-const developerMode = true;
+const developerMode = false;
 const minLatitude = 39.1423;
 const maxLatitude = 39.144609;
 const minLongitude = -77.419817;
