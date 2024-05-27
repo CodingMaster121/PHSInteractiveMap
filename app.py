d0e11076ea4948257cdf5db04f5177145c926e4a
CodingMaster121 <ytryanlo@gmail.com>
2024-05-27 00:24:36 -0400

    average skl issue

--- app.py
+++ app.py
@@ -110,8 +110,7 @@ def generate_directions():
             if teacher.lower() != "teacher" and teacher.lower() == room_value.lower():
                 room_found = True
     else:
-        # room_data = [str(room["room_value"].lower()) for room in rooms]
-        room_data = []
+        room_data = [str(room["room_value"]).lower() for room in rooms]
         if room_value.lower() in room_data:
             directions["directions"].append(room_value)
 
