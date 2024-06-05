af2f9800a29b77245a6746488afeca6c6e2c6a80
CodingMaster121 <ytryanlo@gmail.com>
2024-06-05 00:23:03 -0400

    making a paginator

--- static/get_started.js
+++ static/get_started.js
@@ -1,3 +1,38 @@
-function goBackPage() {
+var currentPage = 1;
+changePage();
 
+function changePage() {
+    var goBackButton = document.getElementById("goBack")
+    var nextButton = document.getElementById("next");
+
+    if(currentPage == 1) {
+        goBackButton.disabled = true;
+    } else {
+        goBackButton.disabled = false;
+    }
+
+    if(currentPage == 7) {
+        nextButton.disabled = true;
+    } else {
+        nextButton.disabled = false;
+    }
+
+    for(var i = 1; i <= 7; i++) {
+        var tutorialPage = document.getElementById("step_" + i);
+        if(i == currentPage) {
+            tutorialPage.style.display = "flex";
+        } else {
+            tutorialPage.style.display = "none";
+        }
+    }
+}
+
+function goBackPage() {
+    currentPage--;
+    changePage();
+}
+
+function nextPage() {
+    currentPage++;
+    changePage();
 }
